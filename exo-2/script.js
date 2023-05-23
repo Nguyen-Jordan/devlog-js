a743abd9bb033b7abc2ee98b5548b0af33019179
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 16:32:13 +0200

    Ex 2 - Les conditions

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,5 +1,7 @@
 // you can write js here
-const myDate = new Date(2023,04,22,9);
+const isTesting = true;
+
+const myDate = new Date();
 
 const week = "Nous sommes en pleine semaine aujourd'hui.";
 const weekEnd = "Nous sommes en weekend aujourd'hui."
@@ -9,10 +11,25 @@ const hour = myDate.getHours();
 
 function isWeekend(date) {
     if (dayOfWeek > 5 || dayOfWeek == 5 && hour > 16 || dayOfWeek == 1 && hour < 9) {
-    console.log(weekEnd);
-} else {
-    console.log(week);
-}
+        console.log(weekEnd);
+    } else {
+        console.log(week);
+    }
 }
 
-console.log(isWeekend(myDate));
+function testing (){
+    if (isTesting){
+        const hour = prompt("Donner une heure (0 - 24)");
+        const day = prompt("Donner un jour (1 - 31)");
+        const month = prompt("Donner un mois en chiffre entier (1 - 12)") -1;
+        const year = prompt("Donner une année")
+        const myDate = new Date(year,month,day,hour);
+    
+        console.log(isWeekend(myDate));
+    } else {
+        console.log(isWeekend(myDate));
+    }
+}
+
+
+console.log(testing(myDate));
