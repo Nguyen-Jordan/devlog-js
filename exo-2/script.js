f22f65028117efe89fe625a4d4107d8f5c920b73
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 15:38:40 +0200

    part 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -4,4 +4,14 @@ const myDate = new Date();
 const week = "Nous sommes en pleine semaine aujourd'hui.";
 const weekEnd = "Nous sommes en weekend aujourd'hui."
 
+const dayOfWeek = myDate.getDay();
+
+function isWeekend(date) {
+    if (dayOfWeek >5){
+        console.log(weekEnd);
+    } else {
+        console.log(week);
+    }
+}
+
 console.log('exo-2');
