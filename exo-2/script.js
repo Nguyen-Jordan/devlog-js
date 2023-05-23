23eae2aa810dec9e0af322c7655bb1000de3006d
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 15:34:35 +0200

    part 1

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,7 @@
 // you can write js here
+const myDate = new Date();
+
+const week = "Nous sommes en pleine semaine aujourd'hui.";
+const weekEnd = "Nous sommes en weekend aujourd'hui."
+
 console.log('exo-2');
