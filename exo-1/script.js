5df6ff580d192ec33a1cc42567691bd1d05204c1
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 13:53:30 +0200

    part 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,4 +1,5 @@
 // you can write js here
 let kelvins = 294;
+let celsius = kelvins - 273; // Pour calculer la température en Celsius on soustrait 273 à la température kelvin.
 
 console.log('hello from file');
