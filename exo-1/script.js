3372eca74b72a1e1679c4d363363d8c874607852
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 14:16:45 +0200

    Ex 1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,8 +1,7 @@
-// you can write js here
-let kelvins = 294;
-let celsius = kelvins - 273; // pour calculer la température en Celsius on soustrait 273 à la température kelvin.
-let fahrenheits = celsius * (9/5) + 32; // pour calculer la empérature en Fahrenheits il faut multiplier la temperapour calculer la température en Fahrenheits il faut multiplier la température Celsius par 1.8 (9/5) et additionner 32.ture celsius par 1.8 (9/5) et additioner 32.
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+const celsius = kelvins - 273; // pour calculer la température en Celsius on soustrait 273 à la température kelvin.
+const fahrenheits = celsius * (9/5) + 32; // pour calculer la empérature en Fahrenheits il faut multiplier la temperapour calculer la température en Fahrenheits il faut multiplier la température Celsius par 1.8 (9/5) et additionner 32.ture celsius par 1.8 (9/5) et additioner 32.
 
-let fahrenheitsFloor = Math.floor(fahrenheits); // je déclare une variable et prend comme valeur la valeur de fahrenheits et il arondit grace à la méthode statique Math.floor().
+const fahrenheitsFloor = Math.floor(fahrenheits); // je déclare une variable et prend comme valeur la valeur de fahrenheits et il arondit grace à la méthode statique Math.floor().
 
 console.log(fahrenheitsFloor);
