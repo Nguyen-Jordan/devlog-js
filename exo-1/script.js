db01f4a14f91cc0cee7d22f29d45a4f741c52d43
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-23 13:59:10 +0200

    part 3

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,5 +1,6 @@
 // you can write js here
 let kelvins = 294;
-let celsius = kelvins - 273; // Pour calculer la température en Celsius on soustrait 273 à la température kelvin.
+let celsius = kelvins - 273; // pour calculer la température en Celsius on soustrait 273 à la température kelvin.
+let fahrenheits = celsius * (9/5) + 32; // pour calculer la empérature en Fahrenheits il faut multiplier la temperapour calculer la température en Fahrenheits il faut multiplier la température Celsius par 1.8 (9/5) et additionner 32.ture celsius par 1.8 (9/5) et additioner 32.
 
-console.log('hello from file');
+console.log(fahrenheits);
