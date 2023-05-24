65f25090fad901704b703e200b89ebda75579ede
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-24 09:32:15 +0200

    part 5

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -5,8 +5,19 @@ function getPlayerChoice(playerInput){
     if(playerInput == 'pierre' || playerInput == 'feuille' || playerInput == 'ciseau'){
         console.log(playerInput);
     } else {
-        console.log("Error : Refresh and try again!")
+        console.log("Error of choice: Refresh and try again!")
     }
 }
 
-console.log(getPlayerChoice(playerInput));
+function getComputerChoice (){
+    const result = Math.floor(Math.random() * 3);
+    if (result === 0){
+        return 'pierre';
+    } else if (result === 1){
+        return 'feuille';
+    } else {
+        return 'ciseau';
+    }
+}
+
+console.log(getComputerChoice());
