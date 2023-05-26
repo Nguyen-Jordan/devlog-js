0748f7871ee1e039c972f59df03585ce5f5825c8
Nguyen-Jordan <dizorder3@gmail.com>
2023-05-26 16:12:40 +0200

    feat(exp-7): add part 2 & 3

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,5 +1,14 @@
 console.log("exo-7");
 
+let translate = {
+    "car": "voiture",
+    "house": "maison",
+    "game": "jeux",
+    "videogame": "jeux-video",
+    "show": "spectacle"
+}
+
 for (let i=0;i<jsonDatas.length;i++){
+    jsonDatas[i].translateType = translate[jsonDatas[i].type];
     console.log(jsonDatas[i]);
 }
